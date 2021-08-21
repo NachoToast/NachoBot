e276a89fe45d09a1e20a544732a765dccccaa3ac
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-08-21 21:55:02 +1200

    Query discord mention support, lowercase.

--- src/commands/minecraft/whitelist/query.ts
+++ src/commands/minecraft/whitelist/query.ts
@@ -3,28 +3,41 @@ import Application from '../../../interfaces/Application';
 import Command, * as Types from '../../../interfaces/Command';
 import isValidUsername from './isValidMinecraftUsername.module';
 import moment from 'moment';
+import filterMessage from '../../../modules/mentionFilter.module';
 
 const query: Command = {
     execute: async ({ message, args = [] }: { message: Types.Message; args: string[] }) => {
         if (args[1] !== undefined) {
-            if (!isValidUsername(args[1])) {
-                message.channel.send(`'${args[1].replace('@', '')}' is not a valid Minecraft username.`);
+            let searchTerm: string;
+
+            if (args[1].includes('<@')) {
+                // args[1] is a discord mention
+                searchTerm = args[1].replace(/[<@!>]/g, '');
+            } else if (isValidUsername(args[1])) {
+                // args[1] is an mc username
+                searchTerm = args[1].toLowerCase();
+            } else {
+                // args[1] is defined but invalid
+                message.channel.send(`'${filterMessage(args[1])}' is not a valid Minecraft username.`);
                 return;
             }
 
-            const application: Application = await User.findOne({ minecraft: args[1] });
+            const application: Application = await User.findOne({
+                $or: [{ discord: searchTerm }, { minecraft: searchTerm }],
+            });
 
             if (application === null) {
-                message.channel.send(`Couldn't find any request linked to user '${args[1].replace('@', '')}'.`);
+                message.channel.send(`Couldn't find any request linked to user '${args[1]}'.`);
             } else {
                 message.channel.send(
-                    `User ${args[1].replace('@', '')} currently has a ${application.status} request linked to Discord account <@${
-                        application.discord
-                    }>, made ${moment(application.applied).fromNow()}.`
+                    `Minecraft user ${application.minecraft} currently has a ${
+                        application.status
+                    } request linked to Discord account <@${application.discord}>, made ${moment(application.applied).fromNow()}.`
                 );
             }
             return;
         }
+        // args[1] is not defined = query own discord id
 
         const application: Application = await User.findOne({ discord: message.author.id });
 
