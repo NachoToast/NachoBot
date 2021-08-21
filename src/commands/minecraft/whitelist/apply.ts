ab5a38c9955bc6fb9a4e89e94ad02ee8adab10a3
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-08-21 21:30:19 +1200

    Force lowercase Minecraft username apply.

--- src/commands/minecraft/whitelist/apply.ts
+++ src/commands/minecraft/whitelist/apply.ts
@@ -2,6 +2,7 @@ import Command, * as Types from '../../../interfaces/Command';
 import User from '../../../models/user';
 import { devMode, modules } from '../../../config.json';
 import isValidUsername from './isValidMinecraftUsername.module';
+import filterMessage from '../../../modules/mentionFilter.module';
 
 const notifyNew: boolean = modules.minecraft.whitelist.sendNewApplications;
 const feedChannel: string = devMode
@@ -10,7 +11,7 @@ const feedChannel: string = devMode
 
 const apply: Command = {
     execute: async ({ message, args = [], client }: { message: Types.Message; args: string[]; client: Types.Client }) => {
-        if (args.length < 1 || args[0].includes('@')) {
+        if (args.length < 1) {
             message.channel.send('Please specify your Minecraft username.');
             return;
         }
@@ -18,33 +19,35 @@ const apply: Command = {
         const minecraftUsername = args[0];
 
         if (!isValidUsername(minecraftUsername)) {
-            message.channel.send(`'${minecraftUsername.replace('@', '')}' is not a valid Minecraft username.`);
+            message.channel.send(`'${filterMessage(minecraftUsername)}' is not a valid Minecraft username.`);
             return;
         }
 
         const discordID = message.author.id;
-        const existingUser = await User.findOne({ $or: [{ minecraft: minecraftUsername }, { discord: discordID }] });
+        const existingUser = await User.findOne({
+            $or: [{ minecraft: minecraftUsername.toLowerCase() }, { discord: discordID }],
+        });
 
         if (!devMode && existingUser) {
             if (existingUser.discord === discordID) {
                 message.channel.send(
-                    `You already have a *${existingUser.status ?? 'unknown'}* whitelist application for ${
+                    `You already have a *${existingUser.status ?? 'unknown'}* whitelist application linked to Minecraft user' ${
                         existingUser.minecraft
-                    }.`
+                    }'.`
                 );
             } else {
-                message.channel.send(`${minecraftUsername} has already applied via another Discord account.`);
+                message.channel.send(`Minecraft user '${minecraftUsername}' has already applied via another Discord account.`);
             }
             return;
         }
 
         User.create({
-            minecraft: minecraftUsername,
+            minecraft: minecraftUsername.toLowerCase(),
             discord: discordID,
             applied: new Date().toISOString(),
         });
 
-        message.channel.send(`Successfully submitted a whitelist application for '${minecraftUsername}'`);
+        message.channel.send(`Successfully submitted a whitelist application for Minecraft user '${minecraftUsername}'.`);
 
         if (notifyNew) {
             const outputChannel: any | undefined = client.channels.cache.get(feedChannel);
