fb2cc9849e19deafc7629c80ba99aa61215c9e7c
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-09-13 19:30:55 +1200

    update jsonc

---
+++ src/modules/moduleManager.module.ts
@@ -0,0 +1,11 @@
+import * as config from '../config.json';
+
+const MODULES: {
+    [index: string]: any;
+} = {};
+
+console.log(`[Module Manager] Importing Modules...`);
+
+if (true) {
+    // import fs from 'fs';
+}
